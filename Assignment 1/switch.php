920015ae9ec91d89bbbd12d96a21091da11d6dc4
Bryan Camayang <bryancamayang01@gmail.com>
2026-08-17 13:19:46 +0800

    Create switch.php

---
+++ Assignment 1/switch.php
@@ -0,0 +1,26 @@
+<?php
+$time = "Morning";
+
+switch ($time){
+    case "Dawn":
+        echo "It is Dawn";
+        break;
+
+    case "Morning":
+        echo "It is Morning";
+        break;
+    
+    case "Afternoon":
+        echo "It is Afternoon";
+        break;
+    
+    case "Evening":
+        echo "It is Evening";
+        break;
+
+    case "Midnight":
+        echo "It is Midnight";
+        break;
+}
+
+?>

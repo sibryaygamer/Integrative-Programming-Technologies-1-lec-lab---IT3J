095d8c92c2c616cd94fcd4cfc723d66026522942
Bryan Camayang <bryancamayang01@gmail.com>
2026-08-17 13:19:05 +0800

    Add IF_ELSEIF_ELSE.php for score evaluation

---
+++ Assignment 1/IF_ELSEIF_ELSE.php
@@ -0,0 +1,14 @@
+<?php
+    $score = 100;
+
+    if ($score < 100){
+        echo  "Passed";
+    }
+
+    else if ($score == 99){
+        echo  "Passed";
+    }
+    else{
+        echo "Perfect";
+    }
+?>
